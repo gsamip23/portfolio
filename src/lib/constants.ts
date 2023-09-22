d6d4309fd8818eca67ba2202298bad0e4aada852
Samip Ghimire <gsamip23@gmail.com>
2023-09-22 23:48:17 +0545

    Second commit

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,6 @@
 export const LINKS = {
-  github: "https://github.com/gsamip23",
+  github: "https://github.com/gsamip23/",
   linkedin: "https://www.linkedin.com/in/samipghimire/",
+  youtube: "https://www.youtube.com/@samipghimire2735"
 
 };
