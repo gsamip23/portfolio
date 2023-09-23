421605628604a450d0791f16ac82fcdd084206a1
pratik kuikel <35728183+pratikkuikel@users.noreply.github.com>
2023-09-23 19:52:03 +0545

    removes netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import netlify from "@astrojs/netlify/functions";
 import robotsTxt from "astro-robots-txt";
 import UnoCSS from "@unocss/astro";
 
@@ -13,13 +12,11 @@ export default defineConfig({
         sitemap(),
         robotsTxt({
             sitemap: [
-                "https://gianmarco.xyz/sitemap-index.xml",
-                "https://gianmarco.xyz/sitemap-0.xml",
+                "https://samipghimire.com.np/sitemap-index.xml",
+                "https://samipghimire.com.np/sitemap-0.xml",
             ],
         }),
         solidJs(),
         UnoCSS({ injectReset: true }),
     ],
-    output: "server",
-    adapter: netlify(),
 });
